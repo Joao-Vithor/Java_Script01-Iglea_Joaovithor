9e659b96b276be4738b1c88fd476f7e9c6ee3cec
Joao-Vithor©sjoor <104327716+Joao-Vithor@users.noreply.github.com>
2023-10-23 14:42:52 -0300

    zj

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+
+    document.querySelector ('#som_tecla_pon').play(); 
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
